a31a8918990acc37248e9f65530abe30fccd9d07
MiN1One <git_min1one@mail.ru>
2024-04-08 12:48:14 +0500

    chore: descrease banner height

--- src/components/Home/Banners/Banners.tsx
+++ src/components/Home/Banners/Banners.tsx
@@ -61,7 +61,7 @@ const Banners: FC<BannerProps> = memo(({ banners }) => {
           slidesPerView={1}
           breakpoints={{
             1025: {
-              slidesPerView: 1.9,
+              slidesPerView: 2,
               spaceBetween: 20,
               centeredSlides: true
             }
