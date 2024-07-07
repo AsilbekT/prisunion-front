9243782fcd5629a0df237b57a8f9bcad7340abdc
MiN1One <git_min1one@mail.ru>
2024-07-08 03:10:29 +0500

    fix: move env to config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import i18nConfig from './next-i18next.config.js';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: i18nConfig.i18n,
+  env: {
+    NEXT_PUBLIC_API_URL: 'http://127.0.0.1:8000',
+  },
 };
 
 export default nextConfig;
